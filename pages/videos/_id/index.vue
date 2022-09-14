51c0f54e27630874683170d7dc7f7497ef2e744e
agwadan <agwa.dan@gmail.com>
2022-09-14 12:41:36 +0300

    add dynamic routes to watch page

--- pages/videos/_id/index.vue
+++ pages/videos/_id/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <h2>I want to watch {{video.title}} again. It was an awesome movie</h2>
+  <div>
+    <h2>I want to watch {{video.title}} again. It was an awesome movie</h2>
+    <nuxt-link :to="`/videos/${video.id}/watch`">
+      <button>Watch</button>
+    </nuxt-link>
+  </div>
 </template>
 
 <script>
--- pages/videos/index.vue
+++ pages/videos/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Videos</h1>
+  <div>
+    <h1>Videos</h1>
+    <div v-for="video in videos" :key="video.id">
+      <h3><nuxt-link :to="`/videos/${video.id}`">{{video.title}}</nuxt-link></h3>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
- 
+  data() {
+    return { 
+      videos: [{
+        id: 1,
+        title: 'The sound of Music'
+      }, {
+        id: 2,
+        title: 'Real Steel'
+      }, {
+        id: 3,
+        title: 'Cars 3'
+      }]
+    }
+  },
+
 }
 </script>
 
